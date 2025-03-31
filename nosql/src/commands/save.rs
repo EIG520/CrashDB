97179cdc5adfe79c6dbcf9e0a4de9795ac7139ba
ZTS 439 <eigaming520@gmail.com>
2025-03-31 12:10:58 -0500

    make other types work

--- nosql/src/commands/save.rs
+++ nosql/src/commands/save.rs
@@ -1,14 +1,28 @@
 use super::commands::NotEnoughArgsError;
-use crate::data_types::{data_types::SavableType, table::Table, int::Int};
+use crate::data_types::{data_types::{Loadable, Savable, SavableType}, int::Int, table::Table};
 use std::{cell::RefCell, rc::Rc};
 
 impl Table {
     pub fn handle_set<'a>(&mut self, mut cmd: impl Iterator<Item=&'a str>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {        
+        let key = cmd.next().ok_or(NotEnoughArgsError {})?.to_owned();
         
+        let string_val = cmd.next().ok_or(NotEnoughArgsError {})?.to_owned();
+        let mut typ = 0;
+
+        if let Ok(ty) = self.load(key.clone()) {
+            typ = ty.borrow().signature();
+        }
         
+        let val = match typ {
+            0 => SavableType::String(string_val),
+            1 => SavableType::Table(Table::from_str(&string_val)),
+            2 => SavableType::Int(Int::from_str(&string_val)), 
+            _ => {SavableType::String(string_val)}
+        };
+
         self.save(
-            cmd.next().ok_or(NotEnoughArgsError {})?.to_owned(),
-            Rc::new(RefCell::new(SavableType::String(cmd.next().ok_or(NotEnoughArgsError {})?.to_owned())))
+            key,
+            Rc::new(RefCell::new(val))
         )?;
 
 
